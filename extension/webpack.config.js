7a1044394e6dc103bb4b2313d3d034357f00d746
Alex Ezell <alex@alexezell.com>
2025-07-23 17:03:48 -0500

    Move dist folders around

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist/chrome'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true
   },
   module: {
